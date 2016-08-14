a4ad0085b32449fcd143b31f8c47d7cc7e388d91
Julian Biebelberg <jbiebelberg@gmail.com>
2016-08-14 09:53:36 -0400

    corrected picture error

---
+++ server.js
@@ -0,0 +1,76 @@
+var express = require('express');
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: "nj5rh9gto1v5n05t.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    port: 3306,
+    user: "jpmfic6bzhhzeyjd", //Your username//
+    password: "az0rql713ckr5hef", //Your password//
+    database: "jycxtsgajvvqqqrx"
+})
+
+connection.connect(function(err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+    }
+    
+})
+
+var app = express();
+
+var exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+
+var bodyParser = require('body-parser')
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+
+app.set('view engine', 'handlebars');
+
+app.post('/', function(req, res){
+	//console.log(req.body.name);
+	var pokemon = req.body.name;
+	connection.query('INSERT INTO pokemon (name) VALUES (?)', pokemon, function(err, result){
+		if (err) throw err;
+	});
+
+})
+
+app.get('/', function(req, res){
+	res.render('index');
+})
+
+app.get('/catch', function(req, res){
+	res.render('catch');
+})
+
+app.get('/pokedex', function(req, res){
+	res.render('pokedex');
+})
+
+app.get('/info', function(req, res){
+	var list = {};
+	var total = [];
+
+	connection.query('SELECT * FROM pokemon;', function(err, data){
+				if (err) throw err;
+				
+				for(i=0; i<data.length; i++){
+					
+					total.push(data[i].name);
+
+				}
+				list.names = total;
+				//console.log(list);
+				res.send(list);
+			});
+
+})
+
+var port = 3000;
+
+app.listen(port);
